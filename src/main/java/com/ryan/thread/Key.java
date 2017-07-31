21bc8af7e3bc08bbf590d063b427a8a09abd472e
ryangui <ryangui@qq.com>
2017-07-31 18:12:53 +0800

    add local and key

---
+++ src/main/java/com/ryan/thread/Key.java
@@ -0,0 +1,10 @@
+package com.ryan.thread;
+
+/**
+ * Created by ryan.gui on 2017/7/31.
+ */
+public interface Key<K> {
+
+    K findKey();
+
+}
---
+++ src/main/java/com/ryan/thread/LocalThread.java
@@ -0,0 +1,7 @@
+package com.ryan.thread;
+
+/**
+ * Created by ryan.gui on 2017/7/31.
+ */
+public class LocalThread {
+}
